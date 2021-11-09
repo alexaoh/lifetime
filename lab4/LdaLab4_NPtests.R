2575387be854ccd97e966179fe9fa7382fd8a300
alexaoh <alexaoh@users.noreply.github.com>
2021-11-09 16:48:55 +0100

    Added code from lab4 (Lecture 09.11.21)

---
+++ lab4/LdaLab4_NPtests.R
@@ -0,0 +1,134 @@
+## =========================================================
+## Lifetime Data Analysis, Course 2021/22
+## 9.11.2021: Nonparametric tests to compare survival curves
+## =========================================================
+load("LdaLab4.RData")
+library(survival)
+
+## Two-sample hypothesis testing
+## =============================
+srem2t <- with(remission, Surv(time, cens) ~ treat)
+svf2t <- survfit(srem2t)
+svf2t
+
+## The survival functions
+## ----------------------
+par(font = 2, font.axis = 2, font.lab = 4, las = 1, mar = c(5, 5, 4, 2))
+plot(svf2t, col = 3:4, xlab = "Time to relapse [Weeks]",
+     lty = 1:2, ylab = expression(bold(hat(S)(t))), lwd = 3, yaxs = "i",
+     bty = "l")
+title("Survival functions among leukemia patients")
+legend("bottomleft", levels(remission$treat), title = "Treatment",
+       bty = "n", col = 3:4, lty = 1:2, lwd = 3)
+
+
+## Comparison of both treatments with respect to the survival function
+## -------------------------------------------------------------------
+
+
+## The survdiff object
+## -------------------
+sdf <- survdiff(srem2t) # Default is rho = 0 --> logrank test. 
+str(sdf)
+sdf$obs; sdf$exp
+sdf$chisq
+
+## Further nonparametric tests
+## ---------------------------
+survdiff(srem2t) # logrank test. 
+survdiff(srem2t, rho = 1) # Peto and Peto (and Prentice) modification of the Gehan-Wilcoxon test. Puts more weight in the beginning. 
+survdiff(srem2t, rho = -1) # Puts more weight on later differences in the survival functions. 
+
+## The Fleming-Harrington class
+## ============================
+library(KMsurv)
+data(kidney)
+head(kidney)
+summary(kidney)
+kidney$type <- factor(kidney$type, labels = c("Surgically", "Percutaneously"))
+
+skid2t <- with(kidney, Surv(time, delta) ~ type)
+survfit(skid2t)
+
+## The figure on Slide 54/103
+## --------------------------
+par(font = 2, font.axis = 2, font.lab = 4, las = 1, mar = c(5, 5, 4, 2))
+plot(survfit(skid2t), col = 3:4, xlab = "Time to infection [Months]",
+     lty = 1:2, ylab = expression(bold(hat(S)(t))), lwd = 3, yaxs = "i",
+     bty = "l")
+title("Survival functions according to catheter type")
+legend("bottomleft", levels(kidney$type), title = "Catheter placement",
+       bty = "n", col = 3:4, lty = 1:2, lwd = 3)
+
+
+## The table on Slide 55/103
+## -------------------------
+survdiff(skid2t)
+survdiff(skid2t, rho = 1)
+
+# install.packages("FHtest")
+library(FHtest)
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+# BiocManager::install("Icens")
+
+## The values of the table on Slide 55/103 (II)
+## --------------------------------------------
+FHtestrcc(skid2t)
+FHtestrcc(skid2t, rho = 1)
+FHtestrcc(skid2t, lambda = 1)
+FHtestrcc(skid2t, rho = 1, lambda = 1)
+FHtestrcc(skid2t, rho = 0.5, lambda = 2)
+
+
+## What about the logrank test with left-truncation?
+## =================================================
+load("../lab3/LdaLab3.RData")
+
+## Conditional survival function (T > 68)
+## --------------------------------------
+sub68 <- subset(channing, aged > 68)
+(sch68 <- survfit(Surv(agee, aged, status) ~ sex, sub68))
+
+par(las = 1, font = 2, font.axis = 2, font.lab = 4, xaxs = "i", yaxs = "i",
+    mar = c(5, 5, 4, 2), bty = "l", cex.lab = 1.5, cex.axis = 1.25)
+plot(sch68, col = 2:3, lwd = 3, xlab = "Age [Years]",
+     xlim = c(65, 102), ylab = expression(bolditalic(hat(S)(t))))
+axis(1, at = seq(60, 100, 5))
+title("Conditional survival functions of people older than 68 years")
+legend("bottomleft", levels(channing$sex), col = 2:3, lwd = 3, bty = "n")
+
+#survdiff(Surv(agee, aged, status) ~ sex, channing) # Right censored data only!
+summary(coxph(Surv(agee, aged, status) ~ sex, channing)) # This works for truncated data (after fitting the cox model).
+# This can hence be used to apply a non-parametric logrank test to left-truncated data.
+
+## Stratified tests
+## ================
+? hodg
+data(hodg)
+head(hodg)
+hodg$gtype <- factor(hodg$gtype, labels = c("Allogeneic", "Autologous"))
+hodg$dtype <- factor(hodg$dtype, labels = c("Non Hodgkin lymphoma",
+                                            "Hodgkins disease"))
+summary(hodg)
+
+shodg <- with(hodg, Surv(time, delta) ~ dtype + gtype)
+survfit(shodg)
+
+# Graphical representation (plot Slide 81/103)
+# --------------------------------------------
+par(font = 2, font.axis = 2, font.lab = 4, las = 1, mar = c(5, 5, 4, 2))
+plot(survfit(shodg), xlab = "Time to death or relapse [Days]",
+     ylab = expression(bold(hat(S)(t))), col = rep(2:3, each = 2),
+     lty = 1:2, lwd = 3, yaxs = "i", bty = "l")
+title("Survival functions of time to death or relapse")
+legend("bottomright", c("Group NHL allo", "Group NHL auto", "Group HOD allo",
+                        "Group HOD auto"),
+       bty = "n", col = rep(2:3, each = 2), lty = 1:2, lwd = 3)
+
+# He says that it is not a good idea to use a stratified test for these data, but show how to fit it nonetheless. 
+# Perhaps there are more comments in the R-file in Atenea. 
+# He says there is an interaction effect, which means that they should be analyzed in each subgroup separetely.
+# The interaction is apparent because: Groupd NHL allo is better then auto (in red), but it is the other way around for the green
+# (in the green the auto is better than allo). Perhaps there is an interaction between the group and the allo/auto (if I understood correctly).
+survdiff(Surv(time,delta) ~ gtype + strata(dtype), hodg) # strata gives stratified test. 
---
+++ lab4/LdaLab4_rmsPackage.R
@@ -0,0 +1,58 @@
+## ===============================================
+## Lifetime Data Analysis, Course 2021/22
+## 9.11.2021: Function survplot of the rms package
+## ===============================================
+library(survival)
+load("LdaLab4.RData")
+ls.str()
+
+srem <- with(remission, Surv(time, cens))
+svf.treat <- survfit(srem ~ treat, remission)
+
+# The plot we have seen already several times
+# -------------------------------------------
+par(font = 2, font.axis = 2, font.lab = 4, las = 1, mar = c(5, 5, 4, 2))
+plot(svf.treat, col = 3:4, xlab = "Time to relapse [Weeks]", mark.time = TRUE,
+     lty = 1:2, ylab = expression(bold(hat(S)(t))), lwd = 3, yaxs = "i",
+     bty = "l", cex = 1.5)
+title("Survival functions according to treatment")
+legend("bottomleft", levels(remission$treat), title = "Treatment",
+       bty = "n", col = 3:4, lty = 1:2, lwd = 3)
+
+# The same curves drawn with function survplot of package rms
+# -----------------------------------------------------------
+library(rms)
+
+svf.treat2 <- npsurv(srem ~ treat, remission)
+svf.treat2
+summary(svf.treat2)
+str(svf.treat2)
+
+# Use of function survplot() instead of plot.survfit()
+# ====================================================
+par(font = 2, font.axis = 2, font.lab = 4, las = 1, mar = c(5, 5, 4, 2))
+survplot(svf.treat2, ylab = expression(bold(hat(S)(t))), col = 1:2, lwd = 3)
+title("Survival functions according to treatment")
+
+# Example 2
+# ---------
+par(font = 2, font.axis = 2, font.lab = 4, las = 1, mar = c(5, 5, 4, 2))
+survplot(svf.treat2, ylab = expression(bold(hat(S)(t))), col = 1:2, lwd = 3,
+         col.fill = grey(c(0.75, 0.85)))
+title("Survival functions according to treatment")
+
+# Example 3
+# ---------
+par(font = 2, font.axis = 2, font.lab = 4, las = 1, mar = c(5, 5, 4, 2))
+survplot(svf.treat2, ylab = expression(bold(hat(S)(t))), col = 1:2, lwd = 3,
+         conf = "none")
+title("Survival functions according to treatment")
+
+# Example 4
+# ---------
+par(font = 2, font.axis = 2, font.lab = 4, las = 1, mar = c(5, 5, 4, 2))
+survplot(svf.treat2, ylab = expression(bold(hat(S)(t))), col = 1:2, lwd = 3,
+         col.fill = grey(c(0.75, 0.85)), n.risk = TRUE)
+title("Survival functions according to treatment")
+
+graphics.off()
