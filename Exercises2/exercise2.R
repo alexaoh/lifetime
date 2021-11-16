cb7f783e1e0f663ee229782523220c98ccfd7862
alexaoh <alexaoh@users.noreply.github.com>
2021-11-16 20:15:03 +0100

    Continued working on Assignment2 in LDA

--- Exercises2/exercise2.R
+++ Exercises2/exercise2.R
@@ -1,7 +1,9 @@
 # Exercise 2 (kidney dataframe from KMSurv)
+library(dplyr)
 library(KMsurv)
 library(survival)
 library(bshazard)
+library(km.ci)
 data(kidney)
 ?kidney
 summary(kidney)
@@ -12,6 +14,7 @@ head(kidney)
 surv1 <- with(kidney, Surv(time, delta)) # Make survival object. 
 summary(surv1)
 sfit1 <- survfit(surv1 ~ type, data = kidney) # Fit the survival curves (estimation with KM).
+sfit1
 summary(sfit1)
 
 plot(sfit1, col = 3:4, xlab = "Time to renal infection [Months]",
@@ -24,5 +27,38 @@ legend("bottomleft", legend = levels(as.factor(kidney$type)), title = "Treatment
 # b) Estimated median times. 
 # sfit1 gives estimated median time for type 1 but not for type 2. Why? This is what the q is in task 1 also!
 
-# c) bshazard for drawing hazard functions. Compare with the survival functions from earlier. 
+# Biased estimate of the median. Is this what they want here?
+# DOES NOT WORK FOR MEDIAN!! FIND HOW I CAN ESTIMATE THE MEDIAN!
+print(sfit1, rmedian = "common")
+print(sfit1, rmedian = 30) 
 
+# c) bshazard for drawing hazard functions. Compare with the survival functions from earlier. 
+hazard1 <- bshazard(with(kidney[kidney$type == 1,], Surv(time, delta)) ~ 1, data = kidney[kidney$type == 1,])
+plot(hazard1)
+hazard2 <- bshazard(with(kidney[kidney$type == 2,], Surv(time, delta)) ~ 1, data = kidney[kidney$type == 2,])
+plot(hazard2)
+
+# Kan jeg beregne Hazardene med Nelson-Aalen for å se om disse gir mening!?
+
+# d) Estimated probabilities of not suffering renal infection after 6, 12, 18, 24 and 30 months?
+surv.group1 <- with(kidney[kidney$type == 1,], Surv(time, delta))
+sfit.group1 <- survfit(surv.group1 ~ type, data = kidney[kidney$type == 1,])
+plot(sfit.group1)
+abline(v=c(6,12,18,24,30))
+summary(sfit.group1) # Kanskje best å bare lese av fra summaryen!
+(surv.times.df <- data.frame(sfit.group1$time, sfit.group1$surv, sfit.group1$lower, sfit.group1$upper))
+surv.times.df[c(5.5, 11.5, 16.5, 23.5, 26.5), ] # 23.5 does not work for some reason, but I can see it.
+
+surv.group2 <- with(kidney[kidney$type == 2,], Surv(time, delta))
+sfit.group2 <- survfit(surv.group2 ~ type, data = kidney[kidney$type == 2,])
+plot(sfit.group2)
+abline(v=c(6,12,18,24,30))
+summary(sfit.group2) # Kanskje best å bare lese av fra summaryen!
+(surv.times.df.2 <- data.frame(sfit.group2$time, sfit.group2$surv, sfit.group2$lower, sfit.group2$upper))
+surv.times.df.2[c(5.5, 11.5, 16.5, 24.5, 28.5, 15.5), ] # Are some NAs because some of them dont exist in summary I think. 
+
+# e) Lower and upper limits of linear EP confidence bands with level 95% at the same levels as d).
+CI.lin.group1 <- km.ci(sfit.group1, conf.level = 0.95, method = "linear")
+summary(CI.lin.group1) # Can see that the confidence intervals are modified!
+CI.lin.group2 <- km.ci(sfit.group2, conf.level = 0.95, method = "linear")
+summary(CI.lin.group2) # Can see that the confidence intervals are modified!
--- Exercises2/exercise4.R
+++ Exercises2/exercise4.R
@@ -1,5 +1,62 @@
 # Exercise 4 (turnover)
+library(FHtest) # Fleming Harrington tests.
+
 setwd("/home/ajo/gitRepos/lifetime/Exercises2")
 load("Assign2Exer4.RData")
 head(turnover)
 summary(turnover)
+
+# a) Draw the survival functions of time until turnover for both men and women. Observations?
+surv1 <- with(turnover, Surv(stag, event)) # Make survival object. 
+summary(surv1)
+sfit1 <- survfit(surv1 ~ gender, data = turnover) # Fit the survival curves (estimation with KM).
+sfit1
+summary(sfit1)
+
+plot(sfit1, col = 3:4, xlab = "Time in company until turnover [Months]",
+     ylab = expression(bolditalic(hat(S)(t))),
+     lty = 1:2, lwd = 3, yaxs = "i", xaxs = "i", bty = "n")
+title("Survival functions according to gender")
+legend("bottomleft", legend = levels(as.factor(turnover$gender)), title = "Gender",
+       bty = "n", col = 3:4, lty = 1:2, lwd = 3)
+
+# b) Survival curves separately for both genders of the supervisor. 
+data.male.supervisor <- turnover[turnover$headgend == "Male", ]
+surv.male.super <- with(data.male.supervisor, Surv(stag, event))
+sfit.male.super <- survfit(surv.male.super ~ gender, data = data.male.supervisor)
+
+plot(sfit.male.super, col = 3:4, xlab = "Time in company until turnover [Months]",
+     ylab = expression(bolditalic(hat(S)(t))),
+     lty = 1:2, lwd = 3, yaxs = "i", xaxs = "i", bty = "n")
+title("Survival functions according to gender with male supervisor")
+legend("bottomleft", legend = levels(as.factor(turnover$gender)), title = "Gender",
+       bty = "n", col = 3:4, lty = 1:2, lwd = 3)
+
+data.female.supervisor <- turnover[turnover$headgend == "Female", ]
+surv.female.super <- with(data.female.supervisor, Surv(stag, event))
+sfit.female.super <- survfit(surv.female.super ~ gender, data = data.female.supervisor)
+
+plot(sfit.female.super, col = 3:4, xlab = "Time in company until turnover [Months]",
+     ylab = expression(bolditalic(hat(S)(t))),
+     lty = 1:2, lwd = 3, yaxs = "i", xaxs = "i", bty = "n")
+title("Survival functions according to gender with female supervisor")
+legend("bottomleft", legend = levels(as.factor(turnover$gender)), title = "Gender",
+       bty = "n", col = 3:4, lty = 1:2, lwd = 3)
+
+# c) Test of hypothesis with Fleming Harrington. Testing the hypothesis:
+# Time to turnover does not depend on the employee's gender.
+skd <- with(turnover, Surv(stag, event) ~ gender)
+FHtestrcc(skd) # rho = 0 and lambda = 0 is logrank?
+FHtestrcc(skd, rho = 1) # Emphasizes earlier differences (lambda = 0) and positive rho.
+FHtestrcc(skd, lambda = 1) # Emphasizes later differences (rho = 0) and positive lambda.
+FHtestrcc(skd, rho = 1, lambda = 1) # Emphasized differences towards the middle (rho = lambda)
+FHtestrcc(skd, rho = 0.5, lambda = 2) # Emphasizes differently also, more towards the end since lambda > rho I think. 
+# Anyhow, none of these tests give significant results to any reasonable level. 
+
+# d) Test the hypothesis with a stratified test. What is the conclusion now. 
+# I am guessing that we take the gender of the supervisor into account here! (The stratification). 
+# Also I am guessing that the conclusion would be different!
+survdiff(Surv(stag, event) ~ gender + strata(headgend), data = turnover) # strata gives stratified test. 
+# Almost or pretty much significant. Is this code correct?
+
+# Comments on differences between the tests? Stratified vs Unstratified, have a look in the theory. 
